e77c7090003776c6a33913ec883a8b06dd521125
James Lay <jameslaymusic@gmail.com>
2016-09-02 16:18:31 -0600

    Replaced ^M with proper newline.

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 
 pdata="$(<index.html)"	#read file into memory
 while :; do
-	ncat -l 8080 <<< """$pdata""" 2>error_log | tee -a request_log
+	ncat -l 8080 <<< """$pdata
+""" 2>error_log | tee -a request_log
 done
